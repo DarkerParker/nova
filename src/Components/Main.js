81c4cbc0d9397ad87bfed333948081837d0b3ee2
Parker Johnson <plj06370@creighton.edu>
2020-08-03 14:00:40 -0500

    base app

---
+++ src/Components/Main.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+// import Title from './Title'
+import PhotoWall from './PhotoWall'
+import AddPhoto from './AddPhoto'
+import Single from './Single'
+import {Route, Link} from 'react-router-dom'
+
+
+class Main extends Component{
+
+    // constructor(){
+    //     super();
+       
+    // }
+
+    state = { loading:true}
+
+    componentDidMount(){
+        this.props.startLoadingPosts().then(() =>{
+            this.setState({loading:false})
+        })
+        this.props.startLoadingComments()
+    }
+
+    render(){
+        return (<div>
+
+                    <h1><Link to="/">PhotoWall</Link></h1>
+                    <Route exact path="/" render={() => (
+
+                        <div>
+                            <PhotoWall {...this.props}/>
+                        </div>
+
+                    )}/>           
+                    
+                    <Route path="/AddPhoto" render={({history}) => 
+                        <AddPhoto {...this.props} onHistory={history}/>
+                    }/>
+
+                    <Route path="/single/:id" render={(params) => 
+                        <Single loading={this.state.loading} {...this.props} {...params}/>
+                    }/>
+
+                </div>)
+    }
+}
+
+export default Main
